562a575725f6fc37111e4eb89ded92ebe7dbe0c9
felix-vierthaler <felix.vierthaler+dev@mailbox.org>
2020-08-18 22:41:30 +0200

    basic frontend functionality working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My own Vue.js App"/>
-  </div>
+  <v-app>
+        <div>
+          <v-toolbar>
+            <v-toolbar-title>Todo Application</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </div>
+
+        <v-row>
+          <v-col sm="3">
+            <v-text-field
+              label="Title"
+              single-line
+              solo
+              v-model="input.title"
+            ></v-text-field>
+          </v-col>
+
+          <v-col sm="6">
+            <v-text-field
+              label="description"
+              single-line
+              solo
+              v-model="input.description"
+            ></v-text-field>
+          </v-col>
+
+          <v-btn class="mx-2" fab dark color="indigo" @click="addTodo">
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+
+        </v-row>
+
+        
+
+        <div>
+          <Todo :key="todo" v-for="todo in todos.slice().reverse()" :title="todo.title" :description="todo.description"/>
+
+        </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Todo
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+  data: () => ({
+    todos: [],
+    input: {},
+    url: 'http://164.90.229.206'
+  }),
+
+  methods: {
+    addTodo() {
+      console.log('submit')
+      axios.post(this.url + '/todos', this.input)
+      .then((response) => {
+        console.log(response)
+        this.fetchTodos()
+      }, (error) => {
+        console.log(error);
+      });
+      this.input = {}
+    },
+
+    fetchTodos() {
+      axios
+      .get(this.url + '/todos')
+      .then(response => (this.todos = response.data))
+    }
+  },
+
+  mounted () {
+    this.fetchTodos()
+  }
+
+};
+</script>
---
+++ client/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="todo">
+      <h2 id="title">{{title}}</h2>
+      {{description}}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'description']
+}
+</script>
+
+<style scoped>
+    #todo{
+        margin: 1em 2em;
+        padding: 1em 2em;
+        background-color: gainsboro;
+    }
+</style>
