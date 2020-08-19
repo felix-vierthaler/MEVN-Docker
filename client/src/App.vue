4c81aa4cf074f9f390cffab1083929e1a4a5e6ac
felix-vierthaler <felix.vierthaler+dev@mailbox.org>
2020-08-19 09:30:18 +0200

    ui fix, auto update, able to delete

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,53 @@
 <template>
   <v-app>
-        <div>
-          <v-toolbar>
-            <v-toolbar-title>Todo Application</v-toolbar-title>
-            <v-spacer></v-spacer>
-          </v-toolbar>
-        </div>
+    <div>
+      <v-toolbar>
+        <v-toolbar-title>MEVN Todo Application</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+    </div>
 
-        <v-row>
-          <v-col sm="3">
-            <v-text-field
-              label="Title"
-              single-line
-              solo
-              v-model="input.title"
-            ></v-text-field>
-          </v-col>
 
-          <v-col sm="6">
-            <v-text-field
-              label="description"
-              single-line
-              solo
-              v-model="input.description"
-            ></v-text-field>
-          </v-col>
+    <div id="content">
+      <v-row align="top">
+        <v-col sm="3">
+          <v-text-field
+            label="Title"
+            single-line
+            solo
+            v-model="input.title"
+          ></v-text-field>
+        </v-col>
 
-          <v-btn class="mx-2" fab dark color="indigo" @click="addTodo">
+        <v-col sm="6">
+          <v-text-field
+            label="description"
+            single-line
+            solo
+            v-model="input.description"
+          ></v-text-field>
+        </v-col>
+
+        <v-spacer></v-spacer>
+
+        <v-col>
+          <v-btn class="mx-4" fab dark medium color="indigo" @click="addTodo">
             <v-icon dark>mdi-plus</v-icon>
           </v-btn>
-
-        </v-row>
-
+        </v-col>
         
 
-        <div>
-          <Todo :key="todo" v-for="todo in todos.slice().reverse()" :title="todo.title" :description="todo.description"/>
+      </v-row>
 
-        </div>
+      <v-divider></v-divider>
+
+      <div>
+        <Todo :key="todo" v-for="todo in todos.slice().reverse()" @delete="deleteTodo(todo._id)" :title="todo.title" :description="todo.description"/>
+      </div>
+
+    </div>
+
+        
   </v-app>
 </template>
 
@@ -63,7 +73,7 @@ export default {
     addTodo() {
       console.log('submit')
       axios.post(this.url + '/todos', this.input)
-      .then((response) => {
+      .then(response => {
         console.log(response)
         this.fetchTodos()
       }, (error) => {
@@ -76,12 +86,29 @@ export default {
       axios
       .get(this.url + '/todos')
       .then(response => (this.todos = response.data))
+    },
+
+    deleteTodo(id) {
+      axios
+      .delete(this.url + '/todos/' + id)
+      .then(response => {
+        console.log(response)
+        this.fetchTodos()
+      }, (error) => {
+        console.log(error);
+      });
     }
   },
 
   mounted () {
-    this.fetchTodos()
+    setInterval(() => { this.fetchTodos() }, 1000);
   }
 
 };
 </script>
+
+<style>
+  #content{
+    margin: 50px;
+  }
+</style>
--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="todo">
-      <h2 id="title">{{title}}</h2>
-      {{description}}
+      <div class="text">
+          <h2 id="title">{{title}}</h2>
+          {{description}}
+      </div>
+      <div class="actions">
+          <v-btn class="mx-4" fab dark medium color="error" @click="$emit('delete')">
+            <v-icon>mdi-minus</v-icon>
+          </v-btn>
+      </div>
+      
+      
   </div>
 </template>
 
 <script>
 export default {
-    props: ['title', 'description']
+    props: ['title', 'description'],
 }
 </script>
 
 <style scoped>
     #todo{
-        margin: 1em 2em;
+        margin: 1em 0;
         padding: 1em 2em;
         background-color: gainsboro;
+
+        display: flex;
+        justify-content: space-between;
     }
 </style>
